2ef37f641a1851873c7810281288154ac0fa1295
alexkarneta <alexkarneta@gmail.com>
2023-01-31 20:13:02 +0300

    feat:hw_1 added solution to task2

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 // task_1:
 (function(){
 	
-	let number=+(prompt('input number (number < 0 && number is Integer)', ''));
+	let number=+(prompt('input number (number > 0 && number is Integer)', ''));
 
 	while(!(number > 0 && Number.isInteger(number))){
-		number=+(prompt('input number (number < 0 && number is Integer)', ''));console.log("Incorrect input!")		
+		number=+(prompt('input number (number < 0 && number is Integer)', ''));
+		console.log("Incorrect input!")		
 	}
 	
 function factorial(num){
@@ -18,7 +19,7 @@ function square(num){
 function isPrime(num){
 	for(let i = 2, s = Math.sqrt(num); i <= s; i++) {
 		if(num % i === 0) return false;
-  }
+  }  
   return num > 1;
 }
 
@@ -36,7 +37,41 @@ function delimeters(num){
 	return result;
 }
 
-console.log(`Number:${number}\nFactorial:${factorial(number)}\nSquare:${square(number)}\nisPrime:${isPrime(number)}\nisEven:${isEven(number)}\nDelimeters:${delimeters(number)}`);
-	
+console.log(`Number:${number}\nFactorial:${factorial(number)}\nSquare:${square(number)}\nisPrime:${isPrime(number)}\nisEven:${isEven(number)}\nDelimeters:${delimeters(number)}`);	
 }())
 
+// task_2:
+(function (){
+
+	let value = prompt('input symbols (1 < symbols <= 3)','');
+	
+	while(!(value.length <= 3 && value.length > 0 && !checkSpaces(value))){
+		value = prompt('input symbols (1 < symbols <= 3)','');
+		console.log("Incorrect input!");
+	}
+	
+	let number=+(prompt('input number (number > 0 && number < 10 && number is Integer )', ''));
+
+	while(!(number > 0 &&  number < 10 && Number.isInteger(number))){
+		number=+(prompt('input number (number < 0 && number is Integer)', ''));
+		console.log("Incorrect input!");
+	}
+
+	let arr=[number];
+	for(let i=0;i<number;i++){
+		arr[i]=new Array
+	}
+	for(let i=0;i<number;i++){
+		for(let j=0;j<number;j++){
+			arr[i][j]=value;
+		}
+	}
+
+	function checkSpaces(val){
+		return /^\s*$/.test(val);
+	}
+
+	const print = arr.map((d) => d.join(" ")).join("\n");
+
+	console.log(print);	
+}())
