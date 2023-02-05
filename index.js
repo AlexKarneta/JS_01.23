1ea4240fc2164b76ab76c00ce967b8d6e9b4e13c
alexkarneta <alexkarneta@gmail.com>
2023-02-05 09:54:23 +0300

    feat:added t3 and fixed t1

--- index.js
+++ index.js
@@ -1,39 +1,83 @@
-function makeDeepCopy(obj){	
-	let reg=/^\s*$/ ;
-	if(obj.match(reg) || !Object.keys(obj).length){
-		throw new Error();
-	}
-	let dublicate = {};
-		
-		for(const i in obj) {
-		  if (obj[i] instanceof Object && !(obj[i] instanceof Array)) {
-			dublicate[i] = makeDeepCopy(obj[i]);
-			continue;
-		  }else{
-			dublicate[i] = obj[i];
-		  }		  
-		}
-		return dublicate;	 
+function makeDeepCopy(obj) {
+  if (!arguments.length || typeof obj !== 'object') {
+    throw new Error();
+  }
+  let duplicate;
+
+  if (Array.isArray(obj) || obj instanceof Map) {
+    duplicate = [];
+  } else {
+    duplicate = {};
+  }
+
+  const ITER_OBJ = obj instanceof Map ? [...obj] : obj;
+
+  for (const prop in ITER_OBJ) {
+    if (typeof ITER_OBJ[prop] == 'object') {
+      duplicate[prop] = makeDeepCopy(ITER_OBJ[prop]);
+    } else {
+      duplicate[prop] = ITER_OBJ[prop];
+    }
+  }
+
+  return obj instanceof Map ? new Map(duplicate) : duplicate;
 }
 
-function selectFromInterval(arr,startIndex,endIndex){
-	if (!Array.isArray(arr) || !isFinite(startIndex) || !isFinite(endIndex) || arr.length === 0) {
-		throw new Error()
-	}
+function selectFromInterval(arr, startIndex, endIndex) {
+  if (
+    !Array.isArray(arr) ||
+    !isFinite(startIndex) ||
+    !isFinite(endIndex) ||
+    arr.length === 0
+  ) {
+    throw new Error();
+  }
 
-	if(arr.every((item)=>typeof(item) =='number')){
-	let res=[startIndex, endIndex];
-	res.sort((a,b)=>a-b)	
-	let start=res[0],end=res[1];
+  if (arr.every((item) => typeof item == 'number')) {
+    let res = [startIndex, endIndex];
+    res.sort((a, b) => a - b);
+    let start = res[0],
+      end = res[1];
 
-	let result=[]
-	for(let item of arr){
-		if(item>=start && item <=end){
-			result.push(item)
-		}
-	}
-	return result;
-	}else throw Error();	
+    let result = [];
+    for (let item of arr) {
+      if (item >= start && item <= end) {
+        result.push(item);
+      }
+    }
+    return result;
+  } else throw new Error();
 }
 
-function createIterable(){}
+function createIterable(from, to) {
+  if (
+    isNaN(from) ||
+    !isFinite(from) ||
+    isNaN(to) ||
+    !isFinite(to) ||
+    to <= from
+  ) {
+    throw new Error();
+  }
+
+  const range = {
+    from: from,
+    to: to,
+  };
+
+  iterObj[Symbol.iterator] = function () {
+    return {
+      current: this.from,
+      last: this.to,
+
+      next() {
+        if (this.current <= this.last) {
+          return { done: false, value: this.current++ };
+        } else {
+          return { done: true };
+        }
+      },
+    };
+  };
+  return range;
+}
