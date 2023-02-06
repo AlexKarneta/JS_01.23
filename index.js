f6818b0648906d049fcd378f62819f676415aba2
alexkarneta <alexkarneta@gmail.com>
2023-02-06 20:05:59 +0300

    fix:t1 refactored

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
-Array.prototype.customFilter = function (arr, cb) {
+Array.prototype.customFilter = function (cb, arg) {
+  if (this == null) {
+    throw new Error();
+  }
   let res = [];
-  for (let i = 0; i < arr.length; i++) {
-    if (cb(arr[i], i, arr)) {
-      res.push(arr[i]);
+  let context = this;
+  let obj = Object(this);
+
+  if (arguments.length > 1) {
+    context = arg;
+  }
+
+  if (typeof cb !== "function") {
+    throw new Error();
+  }
+
+  for (let i = 0; i < obj.length; i++) {
+    if (i in obj) {
+      let current = this[i];
+      if (cb.call(context, current, i, obj)) {
+        res.push(current);
+      }
     }
   }
+
   return res;
 };
